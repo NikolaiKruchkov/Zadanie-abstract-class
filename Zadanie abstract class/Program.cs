b5e913b1ebd1e63c725402405ea847cb4028f023
NikolaiKruchkov <47074017+NikolaiKruchkov@users.noreply.github.com>
2021-11-29 11:26:36 +0300

    Add files via upload

---
+++ Zadanie abstract class/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Zadanie_abstract_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat1 = new Cat();
+            Dog dog1 = new Dog();
+            cat1.Say();
+            dog1.Say();
+            cat1.ShowInfo();
+            dog1.ShowInfo();
+
+        }
+    }
+
+
+
+    abstract class Animal
+    {
+        public abstract string Nazvanie { get; set; }
+        
+        public abstract  void Say();
+        public void ShowInfo()
+        {
+            Console.WriteLine(Nazvanie);
+            Say();
+        }
+        public Animal()
+        {
+            Nazvanie = "Животное";
+        }
+    }
+    class Cat : Animal
+    {
+        string nazvanie;
+        public override string Nazvanie
+        {
+            get
+            {
+                return nazvanie;
+            }
+            set
+            {
+                nazvanie = "Кошка";
+            }
+        }
+        public static string zvuk = "Мяю";
+
+        public override void Say()
+        {
+            Console.WriteLine(Cat.zvuk);
+        }
+    }
+
+    class Dog: Animal
+        {
+            string nazvanie;
+            public override string Nazvanie
+            {
+                get
+                {
+                    return nazvanie;
+                }
+                set
+                {
+                nazvanie = "Собака";
+                }
+            }
+            public static string zvuk = "Гав";
+
+            public override void Say()
+            {
+                Console.WriteLine(Dog.zvuk);
+            }
+
+
+
+
+        }
+    
+
+
+}
